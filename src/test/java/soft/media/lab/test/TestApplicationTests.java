7b3f6aa4f980b19c64ddd2e220efb04a00e63536
Vladimir Fedorchenko <biryuzabear@gmail.com>
2023-10-04 09:34:40 +0400

    Initial commit

---
+++ src/test/java/soft/media/lab/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package soft.media.lab.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
