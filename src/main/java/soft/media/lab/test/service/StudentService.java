78aa7155598e129733bd50ce4d9644ebb94cba89
Vladimir Fedorchenko <biryuzabear@gmail.com>
2023-10-04 22:49:37 +0400

    Дописаны все необъодимые методы в сервисы, чтобы корректно работали контроллеры

--- src/main/java/soft/media/lab/test/service/StudentService.java
+++ src/main/java/soft/media/lab/test/service/StudentService.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import soft.media.lab.test.dto.StudentWithPerformanceDTO;
+import soft.media.lab.test.entity.Performance;
+import soft.media.lab.test.entity.Student;
 import soft.media.lab.test.repository.StudentRepository;
 
 import java.util.List;
@@ -30,14 +32,32 @@ public class StudentService {
 
     @Transactional
     public StudentWithPerformanceDTO createStudent(StudentWithPerformanceDTO studentDTO) {
-        return null;
+        Student student = new Student();
+        return getStudentWithPerformanceDTO(studentDTO, student);
     }
 
+    @Transactional
     public StudentWithPerformanceDTO updateStudent(Long id, StudentWithPerformanceDTO studentDTO) {
-        return null;
+        Student student = studentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Student not found"));
+        return getStudentWithPerformanceDTO(studentDTO, student);
     }
 
-    public void deleteStudent(Long id) {
+    private StudentWithPerformanceDTO getStudentWithPerformanceDTO(StudentWithPerformanceDTO studentDTO, Student student) {
+        student.setFullName(studentDTO.fullName());
+        student.setBirthDate(studentDTO.birthDate());
+        String performanceText = studentDTO.performance();
 
+        if (performanceText != null && !performanceText.isBlank()) {
+            Performance performance = performanceService.getPerformanceByText(performanceText);
+            student.setPerformance(performance);
+        }
+
+        Student savedStudent = studentRepository.save(student);
+        return new StudentWithPerformanceDTO(savedStudent.getFullName(), savedStudent.getBirthDate(), savedStudent.getPerformance().getText());
+    }
+
+    @Transactional
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
     }
 }
