dd76c1e4e49139160cede0ae07520101766d1468
Vladimir Fedorchenko <biryuzabear@gmail.com>
2023-10-04 23:21:49 +0400

    Описаны контроллеры аннотациями Swagger

--- src/main/java/soft/media/lab/test/controller/PerformanceController.java
+++ src/main/java/soft/media/lab/test/controller/PerformanceController.java
@@ -1,5 +1,10 @@
 package soft.media.lab.test.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import soft.media.lab.test.dto.PerformanceDTO;
@@ -9,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/performances")
+@Tag(name = "Performance", description = "API for managing performance")
 public class PerformanceController {
     private final PerformanceService performanceService;
 
@@ -16,13 +22,37 @@ public class PerformanceController {
         this.performanceService = performanceService;
     }
 
+    @Operation(
+            summary = "Get a list of all performances",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list"),
+                    @ApiResponse(responseCode = "500", description = "Server error")
+            }
+    )
     @GetMapping
     public List<PerformanceDTO> getAllPerformances() {
         return performanceService.getAllPerformances();
     }
 
+    @Operation(
+            summary = "Update an existing performance",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully updated performance"),
+                    @ApiResponse(responseCode = "404", description = "Performance not found"),
+                    @ApiResponse(responseCode = "403", description = "Value not valid"),
+                    @ApiResponse(responseCode = "500", description = "Server error")
+            }
+    )
     @PutMapping("/{id}")
-    public PerformanceDTO updatePerformance(@PathVariable Long id, @Valid @RequestBody PerformanceDTO performanceDTO) {
+    public PerformanceDTO updatePerformance(
+            @Parameter(description = "ID of the performance to be updated") @PathVariable Long id,
+            @Parameter(
+                    description = "Updated performance data",
+                    examples = {
+                            @ExampleObject(value = "{\"text\":\"Sample performance\"}")
+                    }
+            )
+            @Valid @RequestBody PerformanceDTO performanceDTO) {
         return performanceService.updatePerformance(id, performanceDTO);
     }
 }
--- src/main/java/soft/media/lab/test/controller/StudentController.java
+++ src/main/java/soft/media/lab/test/controller/StudentController.java
@@ -1,5 +1,9 @@
 package soft.media.lab.test.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import soft.media.lab.test.dto.StudentWithPerformanceDTO;
@@ -9,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
+@Tag(name = "Students", description = "API for managing students")
 public class StudentController {
     private final StudentService studentService;
 
@@ -16,26 +21,60 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @Operation(summary = "Get a list of all students")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
     @GetMapping
     public List<StudentWithPerformanceDTO> getAllStudents() {
         return studentService.getAllStudentsWithPerformances();
     }
 
+    @Operation(summary = "Get a specific student by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved student"),
+            @ApiResponse(responseCode = "404", description = "Student not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
     @GetMapping("/{id}")
     public StudentWithPerformanceDTO getStudent(@PathVariable Long id) {
         return studentService.getStudent(id);
     }
 
+    @Operation(summary = "Create a new student")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created student"),
+            @ApiResponse(responseCode = "403", description = "Value not valid"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
     @PostMapping
     public StudentWithPerformanceDTO createStudent(@Valid @RequestBody StudentWithPerformanceDTO studentDTO) {
         return studentService.createStudent(studentDTO);
     }
 
+    @Operation(summary = "Update a student by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated student"),
+            @ApiResponse(responseCode = "404", description = "Student not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "403", description = "Value not valid"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
     @PutMapping("/{id}")
     public StudentWithPerformanceDTO updateStudent(@PathVariable Long id, @Valid @RequestBody StudentWithPerformanceDTO studentDTO) {
         return studentService.updateStudent(id, studentDTO);
     }
 
+    @Operation(summary = "Delete a student by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted student"),
+            @ApiResponse(responseCode = "404", description = "Student not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Server error")
+    })
     @DeleteMapping("/{id}")
     public void deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
